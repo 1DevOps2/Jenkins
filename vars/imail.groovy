3daf2f7f83046ca498446f1e1748b4a50be748c4
Anas Khan <105498424+1DevOps2@users.noreply.github.com>
2022-09-07 22:55:07 -0700

    Update imail.groovy

--- vars/imail.groovy
+++ vars/imail.groovy
@@ -1,9 +1,14 @@
 #!/usr/bin/env groovy
 /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
 def call() {
-    def from = "anas.rehman@technonies.com"
-    def to = "tehchnonies@gmail.com"
-    def emailBody = "Please go to ${env.BUILD_URL} to verify the build."
-    def emailSubject = "[Jenkins] ${env.JOB_NAME} - Build# ${env.BUILD_NUMBER}"
-    sendEmail("${from}","${to}","${emailSubject}","${emailBody}")
+         def mailRecipients = 'anas.rehman@technonies.com'
+                   def jobName = currentBuild.fullDisplayName
+                   //emailext body: '''${SCRIPT, template="groovy-html.template"}''',
+                   emailext body: '''Hello''',
+                       mimeTye: 'text/html',
+                       subject: "[Jenkins] Started ${jobName}",
+                       to: "${mailRecipients}",
+                       replyTo: "${mailRecipients}",
+                       recipientProviders: [[$class: 'CulpritsRecipientProvider']]
 }
+
