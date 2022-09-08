600d827c350c59f2a03d766ca754434e599a5be3
Anas Khan <105498424+1DevOps2@users.noreply.github.com>
2022-09-07 23:08:23 -0700

    Update imail.groovy

--- vars/imail.groovy
+++ vars/imail.groovy
@@ -1,15 +1,19 @@
 #!/usr/bin/env groovy
 /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
 def call() {
-                  def mailRecipients = '1devops2@gmail.com'
-                  def mailSender = 'anas.rehman@technonies.com'
-                   def jobName = currentBuild.fullDisplayName
+//                   def mailRecipients = '1devops2@gmail.com'
+//                   def mailSender = 'anas.rehman@technonies.com'
+//                    def jobName = currentBuild.fullDisplayName
                    //emailext body: '''${SCRIPT, template="groovy-html.template"}''',
-                   emailext body: '''Hello''',
-                       mimeTye: 'text/html',
-                       subject: "[Jenkins] Started ${jobName}",
-                       to: "${mailRecipients}",
-                       from: "${mailSender}"
+                    mail from: "jenkins@diginctechnologies.com",
+                      to: "tehchnonies@gmail.com", 
+                      subject: "APPROVAL REQUIRED FOR $JOB_NAME" , 
+                      body: """Build $BUILD_NUMBER required an approval. Go to http://13.95.115.63:8080/job/aprroval-job/ for more info."""
+//                    emailext body: '''Hello''',
+//                        mimeTye: 'text/html',
+//                        subject: "[Jenkins] Started ${jobName}",
+//                        to: "${mailRecipients}",
+//                        from: "${mailSender}"
                        
                        
 }
